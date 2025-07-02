21fa6a095a4636b8a01d2f2a3143c57c0068d8b9
vrc <vrivera@peersyst.com>
2025-07-02 15:33:37 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '',
+  base: '/',
   plugins: [react()],
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT as string, 10) : 5173,
